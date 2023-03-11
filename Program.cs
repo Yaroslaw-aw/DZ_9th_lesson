3e687b9ab63dead9fbe410dbfd1a56d3f4cb4605
Yaroslaw-aw <manumission@mail.ru>
2023-03-12 01:16:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DZ_9th_lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
